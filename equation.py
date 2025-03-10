d9b388d414b8c89a37aa5ef6c2ba5eab7e9b0ac7
Max Netyaga <max.netyaga@gmail.com>
2025-03-10 14:18:24 +0200

    add interactive mode

--- equation.py
+++ equation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-from dataclasses import dataclass
+from typing import Type, TypeVar
+import readline
 from math import sqrt
 from pathlib import Path
 
@@ -15,15 +16,16 @@ def equation_solver(input: Path | None):
 
     if INPUT isn`t present, program run in interactive mode.
     """
-    params = (2, 4, 2)
+
+    if not input:
+        params = read_params_stdin()
+
     click.echo(f"Equation is: {str_equation(*params)}")
 
     roots = solve_equation(*params)
     click.echo(f"There are {len(roots)} roots")
     for (i, root) in enumerate(roots):
         click.echo(f"x{i+1} = {root:.1f}")
-
-
 
 
 def str_equation(a: float, b: float, c: float):
@@ -43,9 +45,27 @@ def solve_equation(
         x1 = (-b + sqrt(d)) / (2*a)
         x2 = (-b - sqrt(d)) / (2*a)
         roots.extend((x1, x2))
-    
+
     return roots
 
+
+def read_params_stdin() -> tuple[float, float, float]:
+    a = read_param_stdin("a", float)
+    b = read_param_stdin("b", float)
+    c = read_param_stdin("c", float)
+    return (a, b, c)
+
+
+T = TypeVar("T")
+
+
+def read_param_stdin(name, param_type: Type[T]) -> T:
+    while True:
+        try:
+            param = param_type(input(f"{name} = "))
+            return param
+        except ValueError:
+            click.echo("Value is invalid! Try again")
 
 
 if __name__ == '__main__':
